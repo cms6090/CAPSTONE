b18e28f3ce9c15b7f1296db029fb305ac1d9156c
조정현 <emforhs0913@naver.com>
2024-10-07 15:57:06 +0900

    fix: 에러처리 미들웨어 전역 사용
    -app.js에서 모든 라우터들이 사용

--- nodejs/src/app.js
+++ nodejs/src/app.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv';
 import usersRouter from './routes/users.router.js';
 import cors from 'cors';
 
+import errorMiddleware from './middlewares/error.middleware.js';
+
 const app = express();
 const PORT = 3000;
 
@@ -13,8 +15,8 @@ app.use(cors({
   origin: 'http://localhost:3005',  // 클라이언트의 URL을 명시
   credentials: true,  // 쿠키 전송을 허용하려면 이 옵션을 추가
 }));
+app.use(errorMiddleware);
 app.use('/api', [usersRouter]);
-
 app.listen(PORT, () => {
   console.log(PORT, '포트로 서버가 열렸어요!');
 });
